de37f39741a627e9758649afdd89d34d20f3f98d
nika7407 <nikala7407@gmail.com>
2025-02-05 19:07:49 +0400

    1# project init

---
+++ app/src/main/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public final class AppTest{
+
+    @Test
+    public void testAlwaysPasses() {
+        assertTrue(true);
+    }
+
+}
