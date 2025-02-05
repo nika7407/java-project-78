6bdde2373652b4e9ac383169737ac4c4db6d0912
nika7407 <nikala7407@gmail.com>
2025-02-05 21:09:21 +0400

    1# testTest

---
+++ app/src/test/java/AppTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public final class AppTest {
+
+    @Test
+    public void testAlwaysTrue() {
+        Assertions.assertTrue(true);
+    }
+    
+
+
+}
