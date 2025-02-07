576457a238ee8a123b1a7dc1a08867ecb2fb7902
nika7407 <nikala7407@gmail.com>
2025-02-07 19:43:14 +0400

    2# StringTest, Checkstyle, added abstract scheme

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -3,7 +3,6 @@ import hexlet.code.schemas.Validator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
