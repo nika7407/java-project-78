9696e5207dc3599d1b4b4a264b25ebb434dcb063
nika7407 <nikala7407@gmail.com>
2025-02-11 20:29:43 +0400

    6# fixes & checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -8,10 +8,23 @@ import java.util.function.Predicate;
 public abstract class BaseSchema<T>  {
     protected Map<String, Predicate<T>> parameters = new HashMap<>();
     protected Map<String, BaseSchema<String>> schemas = new HashMap<>();
+
+    /**
+     * Adds a validators into the parameters list
+     *
+     *
+     */
     public void add(String parameterName, Predicate<T> parameter) {
         parameters.put(parameterName, parameter);
     }
 
+
+    /**
+     *
+     * checks T object with every validator in the parameters returns false
+     * when one of them is wrong
+     *
+     */
     public boolean isValid(T objectToTest) {
         Set<String> keys = parameters.keySet();
         for (String key : keys) {
